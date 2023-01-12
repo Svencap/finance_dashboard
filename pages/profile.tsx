544a2e6c8de09afb7c418eb05d8eebfbb162a333
Svencap <Ayzekmanduar@mail.ru>
2023-01-13 00:44:18 +0900

    add profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,151 @@
+import { FC, useState } from "react";
+import styles from "../styles/Home.module.scss";
+import Navbar from "./navbar/Navbar";
+import Header from "./header/Header";
+import { Avatar, AvatarBadge, Divider, Switch } from "@chakra-ui/react";
+import { RiEditLine, RiNotification2Line } from "react-icons/ri";
+import { FiInfo } from "react-icons/fi";
+import stylesProfile from "../styles/Profile.module.scss";
+import { HiLanguage } from "react-icons/hi2";
+import { MdOutlineLightMode, MdOutlineContactSupport } from "react-icons/md";
+import { RiProfileLine, RiContactsLine } from "react-icons/ri";
+import { BiLock } from "react-icons/bi";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button,
+} from "@chakra-ui/react";
+
+interface ProfileProps {}
+
+const Profile: FC<ProfileProps> = () => {
+  const [notification, setNotification] = useState<boolean>(true);
+
+  return (
+    <div className={styles.wrapper}>
+      <Navbar />
+      <div className={styles.contentWrapper}>
+        <Header title="Profile" />
+        <div className={stylesProfile.container}>
+          <div className={stylesProfile.userInfo}>
+            <Avatar
+              size="2xl"
+              name="Segun Adebayo"
+              src="https://bit.ly/code-beast"
+            >
+              <AvatarBadge boxSize="50px" bg="#EDF2F7" borderColor="white">
+                <RiEditLine size={25} color="black" />
+              </AvatarBadge>
+            </Avatar>
+            <div className={stylesProfile.city}>Puerto Rico</div>
+            <div className={stylesProfile.orherInfo}>
+              <div>yourEmail@domain.com</div>
+              <div>+01 234 567 89</div>
+            </div>
+          </div>
+          <div className={stylesProfile.userEditInfo}>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <RiProfileLine size={20} />
+                  Edit profile information
+                </div>
+              </div>
+            </div>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <RiNotification2Line size={20} />
+                  Notifications
+                </div>
+                <Switch
+                  size="md"
+                  isChecked={notification}
+                  onChange={() => setNotification(!notification)}
+                />
+              </div>
+            </div>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <HiLanguage size={22} />
+                  Language
+                </div>
+                <Menu>
+                  <MenuButton as={Button} width="5xs" bgColor="inherit" _hover={{ background: 'inherit'}} _active={{ background: 'inherit'}} height="20px" fontWeight={'normal'} >
+                    English
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>English</MenuItem>
+                    <MenuItem>Russian</MenuItem>
+                  </MenuList>
+                </Menu>
+              </div>
+            </div>
+          </div>
+          <div className={stylesProfile.userEditInfo}>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <RiNotification2Line size={22} />
+                  Security
+                </div>
+              </div>
+            </div>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <MdOutlineLightMode size={22} />
+                  Theme
+                </div>
+                <Menu>
+                  <MenuButton as={Button} width="5xs" bgColor="inherit" _hover={{ background: 'inherit'}} _active={{ background: 'inherit'}} height="20px" fontWeight={'normal'} >
+                    Light
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>Light</MenuItem>
+                    <MenuItem>Dark</MenuItem>
+                  </MenuList>
+                </Menu>
+              </div>
+            </div>
+          </div>
+          <div className={stylesProfile.userEditInfo}>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <MdOutlineContactSupport size={22} />
+                  Help & Support
+                </div>
+              </div>
+            </div>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <RiContactsLine size={22} />
+                  Contact us
+                </div>
+              </div>
+            </div>
+            <div className={stylesProfile.item}>
+              <div className={stylesProfile.itemWrapper}>
+                <div className={stylesProfile.itemIconWithText}>
+                  <BiLock size={22} />
+                  Privacy policy
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
