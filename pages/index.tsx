9ed197115a65c3caf8402b774f47e3bf5873363d
Svencap <Ayzekmanduar@mail.ru>
2023-01-21 03:25:28 +0900

    add flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from "../styles/Home.module.scss";
 import axios from "axios";
 import routes from "../routes.js";
 import { useAppDispatch } from "../redux/hooks/redux-hooks";
-import { actions as usersSlice} from "../redux/slices/usersSlice";
+import { actions as usersSlice } from "../redux/slices/usersSlice";
 
 interface User {
   _id: number;
@@ -20,7 +20,7 @@ function HomePage() {
   const router = useRouter();
   const dispatch = useAppDispatch();
 
-  const getNormalalized = (data:any) => {
+  const getNormalalized = (data: any) => {
     const entities = data.reduce((acc: any, item: User) => {
       const { _id, username, email, password, __v } = item;
       acc[item._id] = { id: _id, username, email, password, __v };
@@ -32,12 +32,12 @@ function HomePage() {
   };
 
   useEffect(() => {
-    const { username } = JSON.parse(localStorage.getItem("user") || '{}');
+    const { username } = JSON.parse(localStorage.getItem("user") || "{}");
     const fetchData = async () => {
       const { data } = await axios.post(routes.dataPath(), { username });
       const { ids, entities } = getNormalalized([data]);
       dispatch(usersSlice.addUser({ ids, entities }));
-    }
+    };
     fetchData();
   }, []);
 
@@ -46,7 +46,23 @@ function HomePage() {
       <Navbar />
       <div className={styles.contentWrapper}>
         <Header title="Home" />
-        <div>awdawdawddwadawdawadw</div>
+        <div className={styles.content}>
+          <div className={styles.contentHead}>
+            <div className={styles.assets}>Assets</div>
+            <div className={styles.coinsContainer}>
+              <div className={styles.coins}>1</div>
+              <div className={styles.coins}>2</div>
+              <div className={styles.coins}>3</div>
+              <div className={styles.coins}>4</div>
+              <div className={styles.coins}>5</div>
+              <div className={styles.coins}>6</div>
+            </div>
+          </div>
+          <div className={styles.contentFooter}>
+            <div className={styles.statistic}>Я статистика</div>
+            <div className={styles.markets}>Я маркеты</div>
+          </div>
+        </div>
       </div>
     </div>
   );
