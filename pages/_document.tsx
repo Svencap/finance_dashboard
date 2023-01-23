41575ce6dbac01106c65092a2050da82e40fff81
Svencap <Ayzekmanduar@mail.ru>
2023-01-23 09:52:44 +0900

    add background color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{ height: "100vh", backgroundColor: "#F3F9FF"}}>
         <Main />
         <NextScript />
       </body>
