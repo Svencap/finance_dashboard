310950437b6c0030db75b444fb91a64950c40bf2
Svencap <Ayzekmanduar@mail.ru>
2023-01-23 09:55:08 +0900

    mobile render

--- pages/navbar/Navbar.tsx
+++ pages/navbar/Navbar.tsx
@@ -12,84 +12,164 @@ import NextLink from "next/link";
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
+  const [tablet, setTablet] = useState(false);
   const router = useRouter();
   const logOut = () => {
     localStorage.removeItem("user");
     router.push("/login");
   };
 
+
+
+  const [positionY, setPosition] = useState(0);
+  const [visible, setVisible] = useState(false)
+
+  console.log(positionY);
+
+  useEffect(()=> {
+      const handleScroll = () => {
+         let moving = window.scrollY
+         
+         setVisible(positionY < moving);
+         setPosition(moving)
+      };
+      window.addEventListener("scroll", handleScroll);
+      return(() => {
+         window.removeEventListener("scroll", handleScroll);
+      })
+  })
+
+  console.log(visible);
+  useEffect(() => {
+    if (window.innerWidth < 1060) {
+      setTablet(true);
+    } else setTablet(false);
+
+    const handleResize = () => {
+      if (window.innerWidth < 1060) {
+        setTablet(true);
+      } else setTablet(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.headNav}>
-        <div className={styles.logo}>
-          <Link
-            href="/"
-            className={styles.logo}
-            _hover={{ textDecoration: "none" }}
-          >
-            <Avatar />
-            FinX
-          </Link>
-        </div>
-        <Divider />
-        <nav className={styles.nav}>
-          <ul className={styles.ul}>
-            <li id="1" className={styles.elList}>
-              <Link as={NextLink} href="/" _hover={{ textDecoration: "none" }}>
-                <Button
-                  leftIcon={<RiHome6Fill size={21} />}
-                  width="160px"
-                  justifyContent="flex-start"
-                >
-                  Home
-                </Button>
-              </Link>
-            </li>
-            <li id="2" className={styles.elList}>
+    // Фикс скрола
+    <div className={!visible && positionY ? styles.show : styles.container}>
+      {!tablet ? (
+        <>
+          <div className={styles.headNav}>
+            <div className={styles.logo}>
               <Link
-                as={NextLink}
-                href="/transactions"
+                href="/"
+                className={styles.logo}
                 _hover={{ textDecoration: "none" }}
               >
-                <Button
-                  leftIcon={<AiOutlineTransaction size={21} />}
-                  width="160px"
-                >
-                  {/* <AiOutlineTransaction size={22} /> */}
-                  Transactions
-                </Button>
+                <Avatar />
+                FinX
               </Link>
-            </li>
-            <li id="3" className={styles.elList}>
-              <Link
-                as={NextLink}
-                href="/wallet"
-                _hover={{ textDecoration: "none" }}
+            </div>
+            <Divider />
+            <nav className={styles.nav}>
+              <ul className={styles.ul}>
+                <li id="1" className={styles.elList}>
+                  <Link
+                    as={NextLink}
+                    href="/"
+                    _hover={{ textDecoration: "none" }}
+                  >
+                    <Button
+                      leftIcon={<RiHome6Fill size={21} />}
+                      width="160px"
+                      justifyContent="flex-start"
+                    >
+                      Home
+                    </Button>
+                  </Link>
+                </li>
+                <li id="2" className={styles.elList}>
+                  <Link
+                    as={NextLink}
+                    href="/transactions"
+                    _hover={{ textDecoration: "none" }}
+                  >
+                    <Button
+                      leftIcon={<AiOutlineTransaction size={21} />}
+                      width="160px"
+                    >
+                      {/* <AiOutlineTransaction size={22} /> */}
+                      Transactions
+                    </Button>
+                  </Link>
+                </li>
+                <li id="3" className={styles.elList}>
+                  <Link
+                    as={NextLink}
+                    href="/wallet"
+                    _hover={{ textDecoration: "none" }}
+                  >
+                    <Button
+                      leftIcon={<IoMdWallet size={20} />}
+                      width="160px"
+                      justifyContent="flex-start"
+                    >
+                      Wallet
+                    </Button>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className={styles.navContainer}>
+            <Divider />
+            <div className={styles.footer} onClick={logOut}>
+              <Button
+                leftIcon={<AiOutlineLogout size={22} />}
+                width="160px"
+                justifyContent="flex-start"
               >
-                <Button
-                  leftIcon={<IoMdWallet size={20} />}
-                  width="160px"
-                  justifyContent="flex-start"
-                >
-                  Wallet
-                </Button>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className={styles.navContainer}>
-        <Divider />
-        <div className={styles.footer} onClick={logOut}>
-          <Button
-            leftIcon={<AiOutlineLogout size={22} />}
-            width="160px"
-            justifyContent="flex-start"
+                Log out
+              </Button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div
+          className={styles.mobileContainer}
+        >
+          <div
+            className={styles.mobileNavigate}
           >
-            Log out
-          </Button>
+            <Link as={NextLink} href="/" _hover={{ textDecoration: "none" }}>
+              <Button size="sm">
+                <RiHome6Fill size={22} />
+              </Button>
+            </Link>
+            <Button size="sm">
+              <AiOutlineTransaction size={22} />
+            </Button>
+            <Button size="sm">
+              <IoMdWallet size={22} />
+            </Button>
+          </div>
+          <div>
+            <Link
+              as={NextLink}
+              href="/login"
+              _hover={{ textDecoration: "none" }}
+            >
+              <Button size="sm">
+                <AiOutlineLogout size={22} />
+              </Button>
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
